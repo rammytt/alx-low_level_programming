651ae5a4c12dc260e4a75254814a843e06e942f1
rammytt <suleytt@gmail.com>
2022-09-09 14:22:55 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,3 @@
-Task ###.txt
-Who has access
-System properties
-Type
-Text
-Size
-609 bytes
-Storage used
-609 bytes
-Location
-0x01-variables_if_else_while
-Owner
-Antony Bahati
-Modified
-11:19 AM by Antony Bahati
-Opened
-10:04 PM by me
-Created
-11:20 AM
-No description
-Viewers can download
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -37,7 +15,6 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-  /* your code goes there */
 if (n > 0)
 {
 	printf("%i is positive\n", n);
