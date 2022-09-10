4862d33209d37ca80ddf36ffa21c3fa2cc6a186a
rammytt <suleytt@gmail.com>
2022-09-09 18:08:59 -0700

    jj

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,46 +1,116 @@
+#include <stdlib.h>
+
+
+
+#include <time.h>
+
+
+
 #include <stdio.h>
+
+
 
 
 
 /**
 
-* main - print the string in the put function
 
-*
 
-* Description: using the main function
+ * main - Prints if number is positive, zero or negative
 
-* this program prints "Programming is like building a multilingual puzzle
 
-* Return: 0
 
-*/
+ *
+
+
+
+ * Return: Always (Success)
+
+
+
+ */
+
+
 
 int main(void)
 
+
+
 {
 
-	char c;
 
-	int i;
 
-	long li;
-
-	long long lli;
-
-	float f;
+    int n;
 
 
 
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+    srand(time(0));
 
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 
-	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 
-	return (0);
+    n = rand() - RAND_MAX / 2;
+
+
+
+
+
+    if (n > 0)
+
+
+
+    {
+
+
+
+        printf("%d is positive\n", n);
+
+
+
+    }
+
+
+
+    else if (n == 0)
+
+
+
+    {
+
+
+
+        printf("%d is zero\n", n);
+
+
+
+    }
+
+
+
+    else
+
+
+
+    {
+
+
+
+        printf("%d is negative\n", n);
+
+
+
+    }
+
+
+
+
+
+    return (0);
+
+
+
+}
+
+
 
