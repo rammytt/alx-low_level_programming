e73270a8991e469b5080203580bc8a805646ad66
rammytt <suleytt@gmail.com>
2022-09-09 19:10:51 -0700

    ada

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,42 @@
+/**
+
+  * main - Ommit some alphabet
+
+  *
+
+  * Return: Always (Success)
+
+  */
+
+int main(void)
+
+{
+
+	char c;
+
+
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	{
+
+		if (c != 'e' && c != 'q')
+
+		{
+
+			putchar(c);
+
+		}
+
+	}
+
+
+
+	putchar('\n');
+
+
+
+	return (0);
+
+}
+
