10bc7e663322686dfb1cc4c62f2933c6d33a0bc2
rammytt <suleytt@gmail.com>
2022-09-20 01:15:20 -0700

    olkjga

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+* main - entry point
+* Return: generate a password
+*/
+
+int main(void)
+
+{
+
+	char c;
+
+	int x;
+
+
+	srand(time(0));
+
+	while (x <= 2645)
+
+	{
+
+		c = rand() % 128;
+
+		x += c;
+
+		putchar(c);
+
+	}
+
+	putchar(2772 - x);
+
+	return (0);
+}
